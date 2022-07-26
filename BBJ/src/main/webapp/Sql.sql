34478e5acb21ca9066e0c9fbc687c8374e604535
smhrd <smhrd@119.200.31.60>
2022-07-26 10:49:32 +0900

    웹 서블릿 추가

---
+++ BBJ/src/main/webapp/Sql.sql
@@ -0,0 +1,89 @@
+create table u_member
+(id varchar2(20),
+pw varchar2(20),
+name varchar2(20),
+exercise varchar2(20),
+address1 varchar2(20),
+address2 varchar2(20),
+constraint u_member12_pk primary key(id));
+
+drop table u_member;
+
+create table Calender
+  (id number(20),
+  calenderDate Date,
+  exercise varchar2(20),
+  memo varchar2(100),
+  constraint Calender_id_pk primary key(ID)
+     );
+     
+  CREATE SEQUENCE num_pt
+  START WITH 1
+  INCREMENT BY 1
+  MAXVALUE 10000
+  MINVALUE 1
+  NOCYCLE;
+  
+--DROP SEQUENCE num_YTvideo
+  
+ 
+create table PT
+(PTNB number(20),
+EXERCISE varchar2(20),
+PTNAME varchar2(20),
+ADDRESS1 varchar2(100),
+ADDRESS2 varchar2(100),
+constraint PT_PTNB_pk primary key(PTNB));
+
+
+
+
+
+create table YTvideo(
+VideoNB Number primary key,
+VideoNm Varchar2(250) not null,
+URL Varchar2(250) not null,
+CollectionDate Date not null,
+Exercise Varchar2(100) not null);
+
+
+DROP table YTvideo
+
+
+
+CREATE SEQUENCE num_YTvideo
+  START WITH 1
+  INCREMENT BY 1
+  MAXVALUE 10000
+  MINVALUE 1
+  NOCYCLE;
+  
+  
+  
+ create table Academy(
+AcademyNB Varchar2(100) primary key,
+Academy Varchar2(100) not null,
+Address1 Varchar2(100) not null,
+Address2 Varchar2(100) not null,
+Exercise Varchar2(100) not null);
+
+CREATE SEQUENCE num_Academy
+  START WITH 1
+  INCREMENT BY 1
+  MAXVALUE 10000
+  MINVALUE 1
+  NOCYCLE;
+  
+  
+  
+  
+  
+  
+desc YTvideo;
+  
+select * from YTvideo2
+
+
+insert into YTVIDEO values(num_YTvideo.NEXTVAL,'test','test',SYSDATE,'test')
+  
+  
